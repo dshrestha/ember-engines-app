4149edb2af956f9db6cc219624f4f361714d39aa
Daniel Mertgen <dm@be-magazine.de>
2016-12-22 17:37:29 +0100

    add services

--- app/app.js
+++ app/app.js
@@ -10,7 +10,17 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver
+  Resolver,
+    engines: {
+        thingyFrontend: {
+            dependencies: {
+                services: [
+                    'store',
+                    {'session': 'user-session'}
+                ]
+            }
+        }
+    }
 });
 
 loadInitializers(App, config.modulePrefix);
